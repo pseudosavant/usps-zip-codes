4afe8e5e2967ea1eec5cddfcbd4fb7671bcd1afc
Paul Ellis <paul@ellisfoundation.com>
2017-05-17 16:02:04 -0700

    Replaced Post Office lookup with ZIP Code lookup - First pass used scraping of a site that turned out to only have city/state names for ZIP codes with Post Offices - New one uses an official USPS API to retrieve all ~42k ZIP codes - You must sign up for a USPS API account to use this

--- src/getZips.js
+++ src/getZips.js
@@ -1,99 +1,137 @@
 'use strict';
 
 var Promise = require('bluebird'),
-    cheerio = require('cheerio'),
-    tableparser = require('cheerio-tableparser'),
     fetch = require('node-fetch'),
-    FormData = require('form-data'),
+    xml = require("node-xml-lite"),
     jsonfile = require('jsonfile');
 
 fetch.Promise = Promise; // Use Bluebird with `fetch`
 
+var USPSUserID = process.env.USPSUserID || ''; // You must supply your own. Sign up for free here: https://www.usps.com/business/web-tools-apis/
+var baseUSPSUrl = 'http://production.shippingapis.com/ShippingAPITest.dll?API=CityStateLookup&XML=';
+
 var ZIPCodes = {};
 
-var baseFetchOptions = {
-  headers: {
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063',
-    'referer':'http://webpmt.usps.gov/pmt008.cfm'
-  },
-  method: 'POST',
-  body: ''
-};
+var startTimestamp = Date.now();
 
-function zipFetchOptions(from, to) {
-  var opts = Object.assign({}, baseFetchOptions);
-  var form = new FormData();
-  form.append('FromZip', pad(from));
-  form.append('ToZip', pad(to));
-  opts.body = form;
-  return opts;
+function pad(num, length) {
+  var s = num.toString();
+  var prefix = (s.length < length ? '0'.repeat(length - s.length) : '');
+
+  return prefix + s;
 }
 
-function pad(i) {
-  var s = i.toString();
-  var prefix = (s.length < 3 ? '0'.repeat(3 - s.length) : '');
-
-  return prefix + s;  
-}
-
-function transformTableObject(tableObject) {
-  var t = tableObject;
-
-  for (var i = 1; i < t[0].length; i++) {
-    var zips = t[0];
-    var cities = t[1];
-    var states = t[2];
-
-    ZIPCodes[zips[i]] = {
-      city: cities[i],
-      state: states[i]
-    };
+function ZIPToNumber(ZIP) {
+  if (typeof ZIP === 'number') {
+    return ZIP;
   }
 
-  return ZIPCodes;
+  return parseInt(ZIP, 10);
 }
 
-function processHTML(html) {
-  console.log('Processing HTML')
+function createZIPRequest(ZIPs) {
+  var header = `<CityStateLookupRequest USERID="${USPSUserID}">`;
+    var body = '';
+    var footer = '</CityStateLookupRequest>';
 
-  var $ = cheerio.load(html);
-  tableparser($);
-  var $table = $('table[Summary="A listing of post offices by zip code"]');
+    ZIPs.forEach(function (ZIP, id) {
+      body += `<ZipCode ID="${id}"><Zip5>${ZIP}</Zip5></ZipCode>`;
+    });
 
-  if ($table.length > 0) {
-    var tableData = $table.parsetable(true, true, true);
-    return transformTableObject(tableData);
-  } else {
-    return undefined;
-  }
-}
-
-function writeJSON() {
-  console.log('Writing zipcodes.json');
-  jsonfile.writeFileSync('ZIPCodes.json', ZIPCodes);
+    return baseUSPSUrl + header + body + footer;
 }
 
 function ZIPRequest(opts) {
-  var to = opts.to < 999 ? opts.to : 999;
-  var from = opts.from >= 0 ? opts.from : 0;
+  var from = (opts.from >= 0 ? opts.from : 0);
+  var to = (opts.to < 99999 ? opts.to : 99999);
 
-  return fetch('http://webpmt.usps.gov/pmt009.cfm', zipFetchOptions(from, to))
-    .then(res => res.text())
-    .then(processHTML);
+  var ZIPs = [];
+  for (var i = from; i <= to; i++) {
+    ZIPs.push(pad(i, 5));
+  }
+
+  console.log(`Fetching ZIPs: ${pad(from, 5)} - ${pad(to, 5)}`);
+  var url = createZIPRequest(ZIPs);
+
+  return fetch(url)
+    .then(processZIPResponse);
+}
+
+function processZIPResponse(res) {
+  function transform(xmlObj) {
+    var zipResponse = xmlObj.childs;
+    return {
+      code: zipResponse[0].childs[0],
+      city: zipResponse[1].childs[0],
+      state: zipResponse[2].childs[0]
+    };
+  }
+
+
+
+  var promise = res.text()
+    .then(function (t) {
+      var o = xml.parseString(t);
+      var ZIPs = o.childs;
+
+      if (o.name === 'Error') {
+        throw `Error: ${ZIPs[1].childs[0]}` || 'General USPS Error';
+      } else {
+        ZIPs.forEach(function (rawZIP) {
+          if (rawZIP.name === "ZipCode" && rawZIP.childs && rawZIP.childs[0] && rawZIP.childs[0].name !== "Error") {
+            var ZIP = transform(rawZIP);
+            ZIPCodes[ZIP.code] = {
+              city: ZIP.city,
+              state: ZIP.state
+            };
+          }
+        });
+      }
+    });
+  return promise;
+}
+
+function writeJSON() {
+  var filename = 'ZIPCodes.json';
+  console.log(`Writing ${filename}`);
+  jsonfile.writeFileSync(filename, ZIPCodes);
+}
+
+function performance() {
+  var duration = Date.now() - startTimestamp;
+  var ZIPCount = Object.keys(ZIPCodes).length;
+  var rate = Math.round(duration / ZIPCount);
+  var perf = (Number.isFinite(rate) ? `(${rate} ms/ZIP)` : '');
+
+  var time = `Processing time: ${duration}ms`;
+  var ZIPs = `${ZIPCount} ZIP Codes ${perf}`;
+
+  console.log(time);
+  console.log(ZIPs);
 }
 
 function getZIPs(from, to) {
-  console.log('Loading USPS ZIP Codes');
-  var window = 10;
-  var reqs = [];
+  var batchSize = 5;
+  var ranges = []
+  var concurrency = 10;
+  from = ZIPToNumber(from);
+  to = ZIPToNumber(to);
 
-  for (var i = from; i < to; i += window) {
-    reqs.push(ZIPRequest({from: i, to: i + window}));
+  if (!(from < to)) {
+    console.error('Invalid arguments: `to` must be larger than `from`');
+    return;
   }
 
-  return Promise.all(reqs)
+  for (var i = from; i <= to; i += batchSize) {
+    var batchEnd = Math.min(to, i + (batchSize - 1));
+    ranges.push({ from: i, to: batchEnd });
+  }
+
+  console.log(`Requesting USPS ZIP Codes: ${from} - ${to}`);
+  return Promise.map(ranges, ZIPRequest, { concurrency })
     .then(writeJSON)
-    .catch(console.log.bind(console));
+    .then(performance)
+    .catch(err => console.dir(err, null, 2));
 }
 
-getZIPs(0, 999);
+getZIPs(90210, 90310);
